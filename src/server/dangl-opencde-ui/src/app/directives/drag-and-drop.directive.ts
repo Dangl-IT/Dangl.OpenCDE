7b51e81f66e19c701af293cab3041e26363a5f7a
Georg Dangl <georg@dangl.me>
2021-08-28 22:26:01 +0200

    Add DragAndDropDirective

---
+++ src/server/dangl-opencde-ui/src/app/directives/drag-and-drop.directive.ts
@@ -0,0 +1,40 @@
+import {
+  Directive,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[opencdeDragAndDrop]',
+})
+export class DragAndDropDirective {
+  @HostBinding('class.fileover') fileOver: boolean = false;
+  @Output() fileDropped = new EventEmitter<any>();
+
+  // Dragover listener
+  @HostListener('dragover', ['$event']) onDragOver(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = true;
+  }
+
+  // Dragleave listener
+  @HostListener('dragleave', ['$event']) public onDragLeave(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = false;
+  }
+
+  // Drop listener
+  @HostListener('drop', ['$event']) public ondrop(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = false;
+    const files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      this.fileDropped.emit(files);
+    }
+  }
+}
