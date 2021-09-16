e204a1c6a79e68ce2133983246d9d72b39620a20
Georg Dangl <georg@dangl.me>
2021-09-16 14:23:09 +0200

    Fix custom selected_documents url being returned by CDEs

--- src/client/Dangl.OpenCDE.Client/Controllers/CdeServerCallbackController.cs
+++ src/client/Dangl.OpenCDE.Client/Controllers/CdeServerCallbackController.cs
@@ -2,8 +2,10 @@
 using Dangl.OpenCDE.Client.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Dangl.OpenCDE.Client.Controllers
 {
@@ -24,6 +26,7 @@ namespace Dangl.OpenCDE.Client.Controllers
             [FromQuery(Name = "selected_url")] string selectedUrl)
         {
             selectedDocumentsUrl ??= selectedUrl;
+            selectedDocumentsUrl ??= TryGetSelectedDocumentsUrlFromQueryParametersOrNull();
 
             await _hubContext
                 .Clients
@@ -39,6 +42,39 @@ namespace Dangl.OpenCDE.Client.Controllers
             var content = HtmlTemplateProvider.GetHtmlContent(string.Empty, siteContent, "OpenCDE Document Selection");
 
             return Content(content, "text/html");
+        }
+
+        private string TryGetSelectedDocumentsUrlFromQueryParametersOrNull()
+        {
+            // This was discovered where a CDE returned a link like this:
+            // https://localhost?state=123?selected_url=https://example.com
+            // This had three issues:
+            // 1. The 'state' parameter was preserved from the client, but then the second query parameter was not separated by an
+            //    ampersand '&' but again by a question mark '?'
+            // 2. The second parameter was actually called 'selected_url', but it should have been 'selected_documents_url'
+            // 3. The second parameter was not URL encoded, but it luckily does still work that way😀
+            if (HttpContext.Request.Query.Count == 1
+                && HttpContext.Request.Query.ContainsKey("state"))
+            {
+                var queryParam = HttpContext.Request.Query["state"].Single();
+
+                var cleanedQuery = queryParam
+                    .SkipWhile(c => c != '?')
+                    .Skip(1)
+                    .Select(c => c.ToString())
+                    .Aggregate((c, n) => c + n);
+
+                if (cleanedQuery.StartsWith("selected_url=", System.StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return cleanedQuery[13..]; // "selected_url=".Length = 13
+                }
+                else if (cleanedQuery.StartsWith("selected_documents_url=", System.StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return cleanedQuery[23..]; // "selected_documents_url=".Length = 23
+                }
+            }
+
+            return null;
         }
     }
 }
