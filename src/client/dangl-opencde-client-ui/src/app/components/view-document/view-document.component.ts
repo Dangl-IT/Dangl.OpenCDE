b1bb9cced7b5f31a3379a4f6530661e5720de2e3
Georg Dangl <georg@dangl.me>
2021-10-05 23:29:24 +0200

    Add compatibility layer for servers not on the latest spec

--- src/client/dangl-opencde-client-ui/src/app/components/view-document/view-document.component.ts
+++ src/client/dangl-opencde-client-ui/src/app/components/view-document/view-document.component.ts
@@ -52,26 +52,39 @@ export class ViewDocumentComponent implements OnInit, OnDestroy {
           .get<SelectedDocuments>(getProxyUrl(documentReferenceUrl))
           .subscribe((r) => {
             this.isLoading = false;
-            if (!r.documents || r.documents.length == 0) {
+
+            if (r.documents && r.documents.length > 0) {
+              this.documentVersion = r?.documents[0];
+            } else if ((<any>r).document_references?.length > 0) {
+              // TODO DELETE THIS WORKAROUND
+              // This is a workaround for older servers that haven't implemented the latest spec, as of 05.10.2021
+              this.documentVersion = (<any>r).document_references[0];
+            } else {
+              this.documentMetadata = null;
+              this.documentVersions = null;
+              this.documentVersion = null;
               return;
             }
-            this.documentVersion = r?.documents[0];
 
-            if (this.documentVersion.links.document_version_metadata?.url) {
+            const metadataUrl =
+              // TODO DELETE THIS WORKAROUND
+              // This is a workaround for older servers that haven't implemented the latest spec, as of 05.10.2021
+              this.documentVersion!.links.document_version_metadata?.url ??
+              (<any>this.documentVersion)!.links.metadata?.url;
+            if (metadataUrl) {
               this.http
-                .get<DocumentMetadata>(
-                  getProxyUrl(
-                    this.documentVersion.links.document_version_metadata.url
-                  )
-                )
+                .get<DocumentMetadata>(getProxyUrl(metadataUrl))
                 .subscribe((metadata) => (this.documentMetadata = metadata));
             }
 
-            if (this.documentVersion.links.document_versions?.url) {
+            const versionsUrl =
+              // TODO DELETE THIS WORKAROUND
+              // This is a workaround for older servers that haven't implemented the latest spec, as of 05.10.2021
+              this.documentVersion!.links.document_versions?.url ??
+              (<any>this.documentVersion)!.links.versions?.url;
+            if (versionsUrl) {
               this.http
-                .get<DocumentVersions>(
-                  getProxyUrl(this.documentVersion.links.document_versions.url)
-                )
+                .get<DocumentVersions>(getProxyUrl(versionsUrl))
                 .subscribe((versions) => (this.documentVersions = versions));
             }
           });
@@ -90,6 +103,11 @@ export class ViewDocumentComponent implements OnInit, OnDestroy {
 
     let downloadUrl =
       this.documentVersion?.links?.document_version_download?.url;
+    if (!downloadUrl) {
+      // TODO DELETE THIS WORKAROUND
+      // This is a workaround for older servers that haven't implemented the latest spec, as of 05.10.2021
+      downloadUrl = (<any>this.documentVersion)?.links?.download?.url;
+    }
     if (downloadUrl) {
       this.fileDownloadClient.downloadFile(downloadUrl).subscribe((r) => {
         this.fileSaverService.saveFile(r.data, r.fileName ?? 'file');
