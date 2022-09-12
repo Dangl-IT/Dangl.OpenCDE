f178c503e986db100731e7c744cc2031782c85a2
Georg Dangl <georg@dangl.me>
2022-09-12 17:00:12 +0200

    Update controllers used for internal OpenAPI document

--- src/server/Dangl.OpenCDE.Core/Configuration/OpenCdeApisOnlyOperationProcessor.cs
+++ src/server/Dangl.OpenCDE.Core/Configuration/OpenCdeApisOnlyOperationProcessor.cs
@@ -13,8 +13,11 @@ namespace Dangl.OpenCDE.Core.Configuration
                     typeof(Dangl.OpenCDE.Core.Controllers.Foundations.AuthenticationController),
                     typeof(Dangl.OpenCDE.Core.Controllers.Foundations.CurrentUserController),
                     typeof(Dangl.OpenCDE.Core.Controllers.Foundations.VersionsController),
-                    typeof(Dangl.OpenCDE.Core.Controllers.CdeApi.DocumentSelectionController),
-                    typeof(Dangl.OpenCDE.Core.Controllers.OpenCdeIntegrationController),
+                    typeof(Dangl.OpenCDE.Core.Controllers.CdeApi.OpenCdeDownloadController),
+                    typeof(Dangl.OpenCDE.Core.Controllers.CdeApi.OpenCdeDownloadIntegrationController),
+                    typeof(Dangl.OpenCDE.Core.Controllers.CdeApi.OpenCdeQueryController),
+                    typeof(Dangl.OpenCDE.Core.Controllers.CdeApi.OpenCdeUploadController),
+                    typeof(Dangl.OpenCDE.Core.Controllers.CdeApi.OpenCdeUploadIntegrationController)
                 };
 
             var shouldProcess = validTypes.Contains(context.ControllerType);
