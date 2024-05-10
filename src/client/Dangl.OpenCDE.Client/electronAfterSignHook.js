14516c859833b399a562539f97d68fa3348af9ce
Georg Dangl <georg@dangl.me>
2024-05-10 14:51:26 +0200

    Update call to signing from within Electron app

--- src/client/Dangl.OpenCDE.Client/electronAfterSignHook.js
+++ src/client/Dangl.OpenCDE.Client/electronAfterSignHook.js
@@ -1,31 +1,36 @@
-﻿const { exec } = require('child_process');
-const path = require('path');
+﻿const { exec } = require("child_process");
+const path = require("path");
 
 exports.default = async function (context) {
+  if (process.platform !== "win32") {
+    // Signing works only on Windows
+    return;
+  }
 
-    if (process.platform !== 'win32') {
-        // Signing works only on Windows
-        return;
-    }
+  // We're calling NUKE to sign the output files before they're being packaged
+  // We're hard coding another root directory here so that Nuke won't try to attempt to write to an existing build.log file
+  const tempRootDirectory = path.join(__dirname, "../../nuke2");
+  const nukeCommand = `"./build.cmd" SignExecutables -ExecutablesToSignFolder "${context.appOutDir}" --root "${tempRootDirectory}"`;
+  const rootDirectory = path.join(__dirname, "../../../");
+  const signTask = new Promise((resolve, reject) => {
+    exec(
+      nukeCommand,
+      {
+        cwd: rootDirectory,
+      },
+      (error, stdout, sdterr) => {
+        console.log(stdout);
+        console.log(sdterr);
 
-    // We're calling NUKE to sign the output files before they're being packaged
-    const nukeCommand = `"./build.cmd" SignExecutables -ExecutablesToSignFolder "${context.appOutDir}"`;
-    const rootDirectory = path.join(__dirname, '../../../');
-    const signTask = new Promise((resolve, reject) => {
-        exec(nukeCommand, {
-            cwd: rootDirectory
-        }, (error, stdout, sdterr) => {
-                console.log(stdout);
-                console.log(sdterr);
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
 
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    resolve();
-                }
-        });
-    });
-
-    await signTask;
-}
+  await signTask;
+};
