e2460f45b8d7c31cd0b1589b2b0fba35ce1f7b27
ruslan-minukov <ruslan.minukov@qit.software>
2025-08-11 12:20:38 +0300

    feat: correct bootstrap for client and server

--- src/client/dangl-opencde-client-ui/src/main.ts
+++ src/client/dangl-opencde-client-ui/src/main.ts
@@ -1,5 +1,4 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
 import {
@@ -7,26 +6,10 @@ import {
   DanglIdentityModule,
 } from '@dangl/angular-dangl-identity-client';
 import { JwtRequestValidatorService } from './app/services/jwt-request-validator.service';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 import { NgDanglIconsModule } from 'ng-dangl-icons';
-import {
-  HeaderComponent,
-  FooterComponent,
-} from '@dangl/angular-material-shared';
-import { MatButtonModule } from '@angular/material/button';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatListModule } from '@angular/material/list';
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app/app.component';
 
--- src/server/dangl-opencde-ui/src/main.ts
+++ src/server/dangl-opencde-ui/src/main.ts
@@ -1,31 +1,11 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
-import {
-  HeaderComponent,
-  FooterComponent,
-} from '@dangl/angular-material-shared';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
 import { DanglIdentityModule } from '@dangl/angular-dangl-identity-client';
 import { NgDanglIconsModule } from 'ng-dangl-icons';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatSortModule } from '@angular/material/sort';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AppComponent } from './app/app.component';
 
 if (environment.production) {
@@ -35,28 +15,9 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
-      HeaderComponent,
-      FooterComponent,
-      BrowserModule,
       AppRoutingModule,
-      MatSidenavModule,
-      MatButtonModule,
       DanglIdentityModule,
-      NgDanglIconsModule,
-      MatMenuModule,
-      MatIconModule,
-      MatCardModule,
-      MatTableModule,
-      MatPaginatorModule,
-      FormsModule,
-      MatInputModule,
-      MatSortModule,
-      MatDividerModule,
-      MatFormFieldModule,
-      ReactiveFormsModule,
-      MatProgressBarModule,
-      MatProgressSpinnerModule,
-      MatCheckboxModule
+      NgDanglIconsModule
     ),
     provideAnimations(),
   ],
