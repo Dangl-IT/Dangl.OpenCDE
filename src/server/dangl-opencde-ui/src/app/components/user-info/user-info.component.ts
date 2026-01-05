2ea4e02015a670efba67a2e6d933ac646fa02c35
ruslan-minukov <ruslan.minukov@qit.software>
2026-01-05 11:54:41 +0200

    feat: server: update angular core te v21

--- src/server/dangl-opencde-ui/src/app/components/user-info/user-info.component.ts
+++ src/server/dangl-opencde-ui/src/app/components/user-info/user-info.component.ts
@@ -5,7 +5,7 @@ import { AuthenticationService } from '@dangl/angular-dangl-identity-client';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { NgDanglIconsModule } from 'ng-dangl-icons';
-import { NgIf } from '@angular/common';
+
 import { UserInfo } from 'node_modules/@dangl/angular-dangl-identity-client/models/user-info';
 
 @Component({
--- src/server/dangl-opencde-ui/src/main.ts
+++ src/server/dangl-opencde-ui/src/main.ts
@@ -1,4 +1,8 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import {
+  enableProdMode,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -14,6 +18,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection(),
     importProvidersFrom(
       AppRoutingModule,
       DanglIdentityModule,
